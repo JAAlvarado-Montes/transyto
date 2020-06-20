3c9b30fe839bea67bd85918ecace501f3ce2ce19
JAAlvarado-Montes <jaime-andres.alvarado-montes@students.mq.edu.au>
2020-06-21 00:51:15 +1000

    Reverted all changes in __init__.py

--- transyto/__init__.py
+++ transyto/__init__.py
@@ -2,14 +2,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-from .version import __version__
-from .transyto import *
-from .utils import *
-from .wcs import *
-
-import platform
-import logging
-
 import os
 PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
 MPLSTYLE = '{}/data/transyto.mplstyle'.format(PACKAGEDIR)
@@ -17,9 +9,16 @@ MPLSTYLE = '{}/data/transyto.mplstyle'.format(PACKAGEDIR)
 # By default Matplotlib is configured to work with a graphical user interface
 # which may require an X11 connection (i.e. a display).  When no display is
 # available, errors may occur.  In this case, we default to the robust Agg backend.
+import platform
 if platform.system() == "Linux" and os.environ.get('DISPLAY', '') == '':
     import matplotlib
     matplotlib.use('Agg')
 
+import logging
 log = logging.getLogger(__name__)
 log.addHandler(logging.StreamHandler())
+
+from .version import __version__
+from .transyto import *
+from .utils import *
+from .wcs import *
