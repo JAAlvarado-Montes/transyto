4a18ee8f84f005f610c263e5a78c7a103a169a59
Benjamin van de Vorstenbosch <bencvdv@gmail.com>
2022-11-24 22:19:28 +1100

    tests consistent quotation style

--- transyto/tests/test_codestyle.py
+++ transyto/tests/test_codestyle.py
@@ -20,4 +20,4 @@ def test_conformance(dirs_to_check):
     print(dirs_to_check)
     style.input_dir(dirs_to_check)
     result = style.check_files()
-    assert result.total_errors == 0, "Found code style errors (and warnings)."
+    assert result.total_errors == 0, 'Found code style errors (and warnings).'
